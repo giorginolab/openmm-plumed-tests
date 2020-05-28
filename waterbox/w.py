714e018c70bc30977f5c4a574b2ad8bb40ceff32
tonigi <toni.giorgino@gmail.com>
2020-05-28 15:16:30 +0200

    water test

---
+++ waterbox/w.py
@@ -0,0 +1,61 @@
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import *
+from sys import stdout, exit, stderr
+
+from openmmplumed import PlumedForce
+
+
+prmtop = AmberPrmtopFile('w.prmtop')
+inpcrd = AmberInpcrdFile('w.inpcrd')
+#pdb = PDBFile("w.pdb")
+
+L = 18.774
+box = (
+    Vec3( L, 0, 0) * angstrom,
+    Vec3( 0, L, 0) * angstrom,
+    Vec3( 0, 0, L) * angstrom
+)    
+
+#prmtop.topology.setPeriodicBoxVectors(box)
+
+system = prmtop.createSystem(nonbondedMethod=PME, 
+	nonbondedCutoff=9*angstrom,  constraints=HBonds)
+
+
+
+#r: RESTRAINT ARG=d AT=5 KAPPA=150
+
+plumedScript = """
+UNITS LENGTH=A 
+d: DISTANCE ATOMS=1,4
+r: RESTRAINT ARG=d AT=5 KAPPA=1500
+PRINT ARG=* FILE=COLVAR
+"""
+
+system.addForce(PlumedForce(plumedScript))
+
+integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
+
+req_plt = Platform.getPlatformByName('CUDA')
+
+simulation = Simulation(prmtop.topology, system, integrator, req_plt)
+simulation.context.setPositions(inpcrd.positions)
+
+if inpcrd.boxVectors is not None:
+    simulation.context.setPeriodicBoxVectors(*inpcrd.boxVectors)
+
+simulation.saveState("pre.xml")
+
+print("Minimizing")
+simulation.minimizeEnergy(maxIterations=100)
+simulation.saveState("minimized.xml")
+
+print("Running")
+simulation.reporters.append(PDBReporter('output.pdb', 1000))
+simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
+                    potentialEnergy=True, temperature=True))
+simulation.step(100000)
+
+simulation.saveState("post.xml")
+
